c4f838a1cd905029401433d327c77ba28c839d9c
lutique666 <alefedorus@gmail.com>
2024-11-21 11:50:41 +0300

    2nd

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # blade
+# PHB
+<p><a href="01-Base.html">ГЛАВА 1 - ОСНОВЫ</a></p>
+<p><a href="02-Character.html">ГЛАВА 2 - ПЕРСОНАЖИ</a></p>
+<p><a href="03-Legion.html">ГЛАВА 3 - ЛЕГИОН</a></p>
+<p><a href="06-Provision.html">ГЛАВА 6 - ФАЗА ОБЕСПЕЧЕНИЯ</a></p>
+<p><a href="07-Game principale.html">ГЛАВА 7 - ПРИНЦИПЫ ИГРЫ</a></p>
--- js/content-page.js
+++ js/content-page.js
@@ -1,1 +1,1 @@
-﻿var table_of_contents = '<a href="01-Base.html">ГЛАВА 1 - ОСНОВЫ</a><a href="02-Character.html">ГЛАВА 2 - ПЕРСОНАЖИ</a><a href="03-Legion.html">ГЛАВА 3 - ЛЕГИОН</a>'
+﻿var table_of_contents = '<a href="01-Base.html">ГЛАВА 1 - ОСНОВЫ</a><a href="02-Character.html">ГЛАВА 2 - ПЕРСОНАЖИ</a><a href="03-Legion.html">ГЛАВА 3 - ЛЕГИОН</a><a href="06-Provision.html">ГЛАВА 6 - ФАЗА ОБЕСПЕЧЕНИЯ</a><a href="07-Game principale.html">ГЛАВА 7 - ПРИНЦИПЫ ИГРЫ</a>'
